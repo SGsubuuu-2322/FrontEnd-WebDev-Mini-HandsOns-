76bdc6fc7fd28bba39c6b98fb01e430476caa5b5
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-04-27 00:40:33 +0530

    Development Practice

---
+++ FrontEnd_HandsOn_Projects(HTML&CSS)/PortFolio_WebPage/JS/script.js
@@ -0,0 +1,181 @@
+/*var about = document.getElementById('n-about');
+about.addEventListener('click', function(){
+	var targetPos = 2050;
+    var currentPos = 0;
+
+    var scrollval = setInterval(function(){
+	    if(currentPos >= targetPos){
+		    clearInterval(scrollval);
+		    return;
+	    }
+	    else{
+		    currentPos += 50;
+		    scrollBy(0, 50);
+	    }
+    }, 50);
+});
+
+var skills = document.getElementById('n-skills');
+skills.addEventListener('click', function(){
+	var targetPos = 4700;
+    var currentPos = 0;
+
+    var scrollval = setInterval(function(){
+	    if(currentPos >= targetPos){
+		    clearInterval(scrollval);
+		    return;
+	    }
+	    else{
+		    currentPos += 50;
+		    scrollBy(0, 50);
+	    }
+    }, 50);
+});
+
+var experience = document.getElementById('n-experience');
+experience.addEventListener('click', function(){
+	var targetPos = 6700;
+    var currentPos = 0;
+
+    var scrollval = setInterval(function(){
+	    if(currentPos >= targetPos){
+		    clearInterval(scrollval);
+		    return;
+	    }
+	    else{
+		    currentPos += 50;
+		    scrollBy(0, 50);
+	    }
+    }, 50);
+});
+
+
+var education = document.getElementById('n-education');
+education.addEventListener('click', function(){
+	var targetPos = 12000;
+    var currentPos = 0;
+
+    var scrollval = setInterval(function(){
+	    if(currentPos >= targetPos){
+		    clearInterval(scrollval);
+		    return;
+	    }
+	    else{
+		    currentPos += 50;
+		    scrollBy(0, 50);
+	    }
+    }, 50);
+});
+
+
+var portfolio = document.getElementById('n-portfolio');
+portfolio.addEventListener('click', function(){
+	var targetPos = 17000;
+    var currentPos = 0;
+
+    var scrollval = setInterval(function(){
+	    if(currentPos >= targetPos){
+		    clearInterval(scrollval);
+		    return;
+	    }
+	    else{
+		    currentPos += 50;
+		    scrollBy(0, 50);
+	    }
+    }, 50);
+});
+
+
+var contact = document.getElementById('n-contact');
+contact.addEventListener('click', function(){
+	var targetPos = 29000;
+    var currentPos = 0;
+
+    var scrollval = setInterval(function(){
+	    if(currentPos >= targetPos){
+		    clearInterval(scrollval);
+		    return;
+	    }
+	    else{
+		    currentPos += 50;
+		    scrollBy(0, 50);
+	    }
+    }, 50);
+}); */
+
+
+// var navbarAnchorTags = document.querySelectorAll('.nav-menu a');
+// console.log(navbarAnchorTags);
+
+/*for(var i = 0; i < navbarAnchorTags.length; i++){
+	navbarAnchorTags[i].addEventListener('click', function(event){
+		event.preventDefault();
+		var targetSectionId = this.textContent.trim().toLowerCase();
+		// console.log(targetSectionId);
+		var targetSectionContent = document.getElementById(targetSectionId);
+		// console.log(targetSectionContent); 
+		var interval = setInterval(function(){
+			var targetSectionCo = targetSectionContent.getBoundingClientRect();
+		    console.log(targetSectionCo);
+
+			if(targetSectionCo.top <= 0){
+				clearInterval(interval);
+				return;
+			}
+			window.scrollBy(0, 50);
+		}, 20);
+	});
+}*/
+
+
+// var navbarAnchorTags = document.querySelectorAll('.nav-menu a');
+/*for(var i = 0; i < navbarAnchorTags.length; i++){
+	navbarAnchorTags[i].addEventListener('click', function(event){
+		event.preventDefault();
+		var targetSectionId = this.textContent.trim().toLowerCase();
+		// console.log(targetSectionId);
+		var targetSectionContent = document.getElementById(targetSectionId);
+		// console.log(targetSectionContent); 
+		var interval = setInterval(scrollVertically, 20, targetSectionContent);
+	});
+}
+
+function scrollVertically(targetSectionContent){
+	var targetSectionCo = targetSectionContent.getBoundingClientRect();
+	console.log(targetSectionCo);
+
+	if(targetSectionCo.top <= 0){
+		clearInterval(interval);
+		return;
+	}
+	window.scrollBy(0, 50);
+}*/
+
+var navbarAnchorTags = document.querySelectorAll('.nav-menu a');
+
+for(var i = 0; i < navbarAnchorTags.length; i++){
+	navbarAnchorTags[i].addEventListener('click', function(event){
+		event.preventDefault();
+		var targetSectionID = this.textContent.trim().toLowerCase();
+		// console.log(targetSectionID);
+		var targetSectionContent = document.getElementById(targetSectionID);
+		// console.log(targetSectionContent);
+		console.log(i);
+		var interval = setInterval(scrollVertically, 20, targetSectionContent);
+	});
+}
+
+
+function scrollVertically(targetSectionContent){
+	var targetSectionCo = targetSectionContent.getBoundingClientRect();
+	console.log(targetSectionCo);
+	if(targetSectionCo <= 0){
+		clearInterval(interval);
+		return;
+	}
+	window.scrollBy(0, 50);
+}
+
+
+
+
