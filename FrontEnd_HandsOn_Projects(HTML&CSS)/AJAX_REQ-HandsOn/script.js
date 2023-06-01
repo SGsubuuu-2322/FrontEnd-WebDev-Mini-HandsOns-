ad76faf8c4253d320b338d387886eaacfd00fb51
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-06-02 01:13:20 +0530

    AJAX Request handson

--- FrontEnd_HandsOn_Projects(HTML&CSS)/AJAX_REQ-HandsOn/script.js
+++ FrontEnd_HandsOn_Projects(HTML&CSS)/AJAX_REQ-HandsOn/script.js
@@ -1,17 +1,26 @@
 const btn = $("#fetch-dog-img-btn");
 
 function fetchRandomDogImg() {
-  var xhrReq = new XMLHttpRequest();
+  //   var xhrReq = new XMLHttpRequest();
 
-  xhrReq.onload = function () {
-    // console.log(xhrReq.response);
-    var JSONobj = JSON.parse(xhrReq.response);
-    var imgUrl = JSONobj.message;
-    $("#dog-img").attr("src", imgUrl);
-  };
+  //   xhrReq.onload = function () {
+  //     // console.log(xhrReq.response);
+  //     var JSONobj = JSON.parse(xhrReq.response);
+  //     var imgUrl = JSONobj.message;
+  //     $("#dog-img").attr("src", imgUrl);
+  //   };
 
-  xhrReq.open("get", "https://dog.ceo/api/breeds/image/random", true);
-  xhrReq.send();
+  //   xhrReq.open("get", "https://dog.ceo/api/breeds/image/random", true);
+  //   xhrReq.send();
+
+  $.ajax({
+    url: "https://dog.ceo/api/breeds/image/random",
+    method: "get",
+    success: function (data) {
+      var imgUrl = data.message;
+      $("#dog-img").attr("src", imgUrl);
+    },
+  });
 }
 
 btn.click(fetchRandomDogImg);
