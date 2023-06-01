118e702e8f11a1a09e0538899afd854e548db065
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-06-02 01:18:02 +0530

    AJAX Req handson

--- FrontEnd_HandsOn_Projects(HTML&CSS)/AJAX_REQ-HandsOn/script.js
+++ FrontEnd_HandsOn_Projects(HTML&CSS)/AJAX_REQ-HandsOn/script.js
@@ -1,25 +1,31 @@
 const btn = $("#fetch-dog-img-btn");
 
 function fetchRandomDogImg() {
+  // Step-1:- Through normal js...
   //   var xhrReq = new XMLHttpRequest();
-
   //   xhrReq.onload = function () {
   //     // console.log(xhrReq.response);
   //     var JSONobj = JSON.parse(xhrReq.response);
   //     var imgUrl = JSONobj.message;
   //     $("#dog-img").attr("src", imgUrl);
   //   };
-
   //   xhrReq.open("get", "https://dog.ceo/api/breeds/image/random", true);
   //   xhrReq.send();
 
-  $.ajax({
-    url: "https://dog.ceo/api/breeds/image/random",
-    method: "get",
-    success: function (data) {
-      var imgUrl = data.message;
-      $("#dog-img").attr("src", imgUrl);
-    },
+  //   Step-2 :- Through jquery's 1st method...
+  //   $.ajax({
+  //     url: "https://dog.ceo/api/breeds/image/random",
+  //     method: "get",
+  //     success: function (data) {
+  //       var imgUrl = data.message;
+  //       $("#dog-img").attr("src", imgUrl);
+  //     },
+  //   });
+
+  // Step-3 :- Through jquery's 2nd method...
+  $.get("https://dog.ceo/api/breeds/image/random", function (data) {
+    var imgUrl = data.message;
+    $("#dog-img").attr("src", imgUrl);
   });
 }
 
